7ab9077a08141b7ea008da42717a3e7f4ea955fb
Nguyễn Văn Quyền <vanquyen9900@gmail.com>
2026-03-29 11:05:04 +0700

    requriments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical_chatbot",
+    version="0.1.0",
+    author="Nguyen Van Quyen",
+    author_email="vanquyen9900@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
